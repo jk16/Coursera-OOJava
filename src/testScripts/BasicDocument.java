dc0bf18a1757c115218f44f367264e083660bd42
jk16 <dude16kh@gmail.com>
2016-01-20 16:02:49 -0500

    Class files, my testscripts
    * Added files from the Data structures: Measuring and Optimizing
     		  Performance course
    
    * Created testScripts package with two test classes

---
+++ src/testScripts/BasicDocument.java
@@ -0,0 +1,12 @@
+package testScripts;
+
+public class BasicDocument extends Document {
+	
+	public static void main(String[] args) {
+		BasicDocument BasicDoc = new BasicDocument();
+		String t = BasicDocument.getText();
+		System.out.println(t);
+
+	}
+
+}
---
+++ src/testScripts/Document.java
@@ -0,0 +1,43 @@
+package testScripts;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public abstract class Document {
+    private static String text;
+
+    public Document() {
+    	super();
+    	System.out.println("called ctor");
+        text = "Here is some text. Do we understand it? Let's get excited!";
+    }
+    
+    public static String getText() {
+        return text;
+    }
+
+//    public int getNumWords() {
+//        List<String> tokens = getTokens("[a-zA-Z]+");
+//        return tokens.size();
+//    }
+//
+//    public int getNumSentences() {
+//        //Sentence: Ends with "(.!?)"
+//        List<String> tokens = getTokens("[^!?.]+");
+//        return tokens.size();
+//    }
+//
+//    protected List<String> getTokens(String pattern) {   
+//        ArrayList<String> tokens = new ArrayList<String>();
+//        Pattern tokSplitter = Pattern.compile(pattern);
+//        Matcher m = tokSplitter.matcher(text);
+//        
+//        while (m.find()) {
+//            tokens.add(m.group());
+//        }
+//        
+//        return tokens;
+//    }
+}
