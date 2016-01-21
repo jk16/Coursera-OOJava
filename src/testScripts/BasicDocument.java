e81de5f6797f0cc59e38119a2c1ac0fc2b6405d8
jk16 <dude16kh@gmail.com>
2016-01-20 19:13:17 -0500

    removed print

--- src/testScripts/BasicDocument.java
+++ src/testScripts/BasicDocument.java
@@ -5,7 +5,7 @@ public class BasicDocument extends Document
     public static void main(String[] args) {
         String w = new String("Hello");
         VowelState aVowelState = new VowelState(w);
-        System.out.println(aVowelState.getArrayTuples());
+        System.out.println(aVowelState);
     }
 
 }
